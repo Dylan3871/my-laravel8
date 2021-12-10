28866e46cfcf5da2ed84898c6606d71b584bc7b9
Dylan3871 <al222010139@gmail.com>
2021-12-09 22:53:56 -0600

    correcciones

--- resources/views/estudiantes/form.blade.php
+++ resources/views/estudiantes/form.blade.php
@@ -1,25 +1,24 @@
 
 <label for="alumno_id">Id Alumno</label>
-<input type="text" name="alumno_id" value="{{$estudiantes->alumno_id}}"id="alumno_id">
+<input type="text" name="alumno_id" id="alumno_id">
 <br>
 <label for="nombre">Nombre</label>
-<input type="text" name="nombre" value="{{$estudiantes->nombre}}"id="nombre">
+<input type="text" name="nombre" id="nombre">
 <br>
 <label for="apellidoaaterno">Apellido Paterno</label>
-<input type="text" name="apellidopaterno" value="{{$estudiantes->apellidopaterno}}"id="apellidopaterno">
+<input type="text" name="apellidopaterno" id="apellidopaterno">
 <br>
 <label for="apellidoMaterno">Apellido Materno</label>
-<input type="text" name="apellidoMaterno" value="{{$estudiantes->apellidomaterno}}"id="apellidoMaterno">
+<input type="text" name="apellidoMaterno" id="apellidoMaterno">
 <br>
 <label for="matricula">Matricula</label>
-<input type="text" name="matricula" value="{{$estudiantes->matricula}}"id="matricula">
+<input type="text" name="matricula"  id="matricula">
 <br>
 <label for="foto">Foto</label>
-{{$estudiantes->foto}}
-<input type="file" name="foto" value=""id="foto">
+<input type="file" name="foto" id="foto">
 <br>
 <label for="id_Salon">Id Salon</label>
-<input type="text" name="id_salon" value="{{$estudiantes->id_salon}}id="id_salon">
+<input type="text" name="id_salon" id="id_salon">
 <br>
 <input type="submit"  value="Guardar Datos">
 <br>
--- resources/views/materias/form.blade.php
+++ resources/views/materias/form.blade.php
@@ -1,8 +1,8 @@
 <label for="id_materia">Id Materia</label>
-<input type="text" name="id_materia" value="{{$materia->id_materia}}"id="id_materia">
+<input type="text" name="id_materia" "id="id_materia">
 <br>
 <label for="materia">Materia</label>
-<input type="text" name="materia" value="{{$materia->materia}}" id="materia">
+<input type="text" name="materia"  id="materia">
 <br>
 <input type="submit"  value="Guardar Datos">
 <br>
